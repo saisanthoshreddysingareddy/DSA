b5fbda65b966cb71d961eb29f0c51debaa6108d1
Sai Santhosh Reddy Singareddy <109932454+saisanthoshreddysingareddy@users.noreply.github.com>
2025-11-23 12:20:33 +0530

    Reverse string word

---
+++ Strings/RevereseStringWord.java
@@ -0,0 +1,41 @@
+// Problem Description
+//
+// Given a string A, you are asked to reverse the string
+// and return the reversed string.
+//
+// Problem Constraints
+//   1 <= |A| <= 10^5
+//   A consists only of lowercase characters.
+//
+// Input Format
+//   First and only argument is a string A.
+//
+// Output Format
+//   Return a string denoting the reversed string.
+//
+// Example Input
+// Input 1:
+//   A = "scaler"
+// Input 2:
+//   A = "academy"
+//
+// Example Output
+// Output 1:
+//   "relacs"
+// Output 2:
+//   "ymedaca"
+//
+// Example Explanation
+//   Explanation 1:
+//     Reverse the given string.
+
+
+public class Solution {
+    public String solve(String A) {
+        StringBuilder sb = new StringBuilder();
+        for(int i=A.length()-1; i>=0;i--){
+            sb.append(A.charAt(i));
+        }
+        return sb.toString();
+    }
+}
