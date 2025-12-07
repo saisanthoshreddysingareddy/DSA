c32b4620f213a4154133b42a7dbcfee54d99cc76
Sai Santhosh Reddy Singareddy <109932454+saisanthoshreddysingareddy@users.noreply.github.com>
2025-12-07 18:55:18 +0530

    Find sum of digits

---
+++ Recursion/SumOfDigits.java
@@ -0,0 +1,52 @@
+/*
+Problem Description
+
+Given an integer A, find the sum of its digits using recursion.
+
+Problem Constraints
+  1 <= A <= 10^9
+
+Input Format
+  The first and only argument is an integer A.
+
+Output Format
+  Return an integer denoting the sum of the digits of A.
+
+Example Input
+
+Input 1:
+  A = 46
+
+Input 2:
+  A = 11
+
+Example Output
+
+Output 1:
+  10
+
+Output 2:
+  2
+
+Example Explanation
+
+Explanation 1:
+  Sum of digits of 46 = 4 + 6 = 10
+
+Explanation 2:
+  Sum of digits of 11 = 1 + 1 = 2
+*/
+
+
+public class Solution {
+    public int solve(int A) {
+        int res = findSum(A);
+        return res;
+    }
+    public int findSum(int A){
+        if(A>=1 && A<=9){
+            return A;
+        }
+        return A%10 + findSum(A/10); 
+    }
+}
