4699366be142e0efcd4aff34f31f21b847c22a97
Sai Santhosh Reddy Singareddy <109932454+saisanthoshreddysingareddy@users.noreply.github.com>
2025-12-07 19:23:29 +0530

    Print array

---
+++ Recursion/PrintArray.java
@@ -0,0 +1,42 @@
+/*
+Problem Description
+
+You are given an array A. Your task is to print all elements of the array
+using recursion.
+
+You must not use any loops. However, you may create new helper functions.
+Do not change the signature of the function PrintArray.
+
+Print the array elements in sequential order, followed by a new empty line.
+
+Problem Constraints
+  1 <= |A| <= 100
+  -1000 <= A[i] <= 1000
+
+Input Format
+  One line containing the array A.
+
+Output Format
+  Print all elements of the array in the same order, separated by spaces.
+
+Example Input
+
+Input 1:
+  A = [6, -2, 5, 3]
+
+Input 2:
+  A = [1]
+
+Example Output
+
+Output 1:
+  6 -2 5 3 
+
+Output 2:
+  1 
+
+Example Explanation
+
+Explanation:
+  Print the elements of the array in the order in which they appear.
+*/
