fc7ce08f5fb97a4071c2c305248d17f70aa733b0
Sai Santhosh Reddy Singareddy <109932454+saisanthoshreddysingareddy@users.noreply.github.com>
2025-12-09 16:14:37 +0530

    Create CountDistinctElements.java

---
+++ Hashing/CountDistinctElements.java
@@ -0,0 +1,42 @@
+/*
+Problem Description
+
+Given an array A consisting of N integers, your task is to determine
+the number of unique (distinct) elements present in the array.
+
+Two elements are considered the same if their values are equal.
+
+Problem Constraints
+  1 <= N <= 100000
+  1 <= A[i] <= 10^9
+
+Input Format
+  The first argument is an integer array A.
+
+Output Format
+  Return the number of distinct elements in the array.
+
+Example Input
+
+Input 1:
+  A = [3, 4, 3, 6, 6]
+
+Input 2:
+  A = [3, 3, 3, 9, 0, 1, 0]
+
+Example Output
+
+Output 1:
+  3
+
+Output 2:
+  4
+
+Example Explanation
+
+Explanation 1:
+  The distinct values in the array are {3, 4, 6}. Total = 3.
+
+Explanation 2:
+  The distinct values in the array are {3, 9, 0, 1}. Total = 4.
+*/
