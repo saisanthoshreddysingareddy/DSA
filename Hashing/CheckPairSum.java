e8801f76f16c17854635ef959a85cad36b46157f
Sai Santhosh Reddy Singareddy <109932454+saisanthoshreddysingareddy@users.noreply.github.com>
2025-12-10 20:48:26 +0530

    Check existence of pair sum

--- Hashing/CheckPairSum.java
+++ Hashing/CheckPairSum.java
@@ -56,3 +56,17 @@ Explanation 2:
 */
 
 
+public class Solution {
+    public int solve(int A, ArrayList<Integer> B) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int i=0; i<B.size(); i++){
+            int target = A - B.get(i);
+            if(set.contains(target)){
+                return 1;
+            }else{
+                set.add(B.get(i));
+            }
+        }
+        return 0;
+    }
+}
