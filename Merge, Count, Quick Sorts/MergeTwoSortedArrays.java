9bb396cce74dee52af7c046f19db188d238d6079
Sai Santhosh Reddy Singareddy <109932454+saisanthoshreddysingareddy@users.noreply.github.com>
2025-12-12 16:43:38 +0530

    Create MergeTwoSortedArrays.java

---
+++ Merge, Count, Quick Sorts/MergeTwoSortedArrays.java
@@ -0,0 +1,47 @@
+/*
+Problem Description
+
+Given two sorted integer arrays A and B, merge them into one sorted array
+and return the result.
+
+Note:
+  - A linear time complexity is expected.
+  - You should avoid using any library sorting functions.
+
+Problem Constraints
+  -2 × 10^9 <= A[i], B[i] <= 2 × 10^9
+  1 <= |A|, |B| <= 5 × 10^4
+
+Input Format
+  The first argument is a 1-D integer array A.
+  The second argument is a 1-D integer array B.
+
+Output Format
+  Return a 1-D array representing the merged sorted result of A and B.
+
+Example Input
+
+Input 1:
+  A = [4, 7, 9]
+  B = [2, 11, 19]
+
+Input 2:
+  A = [1]
+  B = [2]
+
+Example Output
+
+Output 1:
+  [2, 4, 7, 9, 11, 19]
+
+Output 2:
+  [1, 2]
+
+Example Explanation
+
+Explanation 1:
+  Merging A and B produces the sorted array [2, 4, 7, 9, 11, 19].
+
+Explanation 2:
+  Merging A and B produces the sorted array [1, 2].
+*/
