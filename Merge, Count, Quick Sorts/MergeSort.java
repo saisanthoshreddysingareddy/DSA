2729194c66a5b708fd87ed629b945ec82ae63243
Sai Santhosh Reddy Singareddy <109932454+saisanthoshreddysingareddy@users.noreply.github.com>
2025-12-12 16:46:27 +0530

    Create MergeSort.java

---
+++ Merge, Count, Quick Sorts/MergeSort.java
@@ -0,0 +1,40 @@
+/*
+Problem Description
+
+Given an array A, sort this array using the Count Sort algorithm
+and return the sorted array.
+
+Problem Constraints
+  1 <= |A| <= 10^5
+  1 <= A[i] <= 10^5
+
+Input Format
+  The first argument is an integer array A.
+
+Output Format
+  Return an integer array that represents the sorted version of A.
+
+Example Input
+
+Input 1:
+  A = [1, 3, 1]
+
+Input 2:
+  A = [4, 2, 1, 3]
+
+Example Output
+
+Output 1:
+  [1, 1, 3]
+
+Output 2:
+  [1, 2, 3, 4]
+
+Example Explanation
+
+For Input 1:
+  The array in sorted order is [1, 1, 3].
+
+For Input 2:
+  The array in sorted order is [1, 2, 3, 4].
+*/
