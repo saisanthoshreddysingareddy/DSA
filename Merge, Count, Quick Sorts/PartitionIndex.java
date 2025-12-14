0ebd5959d6cfa33f96f15cd515547028053590fe
Sai Santhosh Reddy Singareddy <109932454+saisanthoshreddysingareddy@users.noreply.github.com>
2025-12-14 11:47:04 +0530

    Create PartitionIndex.java

---
+++ Merge, Count, Quick Sorts/PartitionIndex.java
@@ -0,0 +1,46 @@
+/*
+Problem Description
+
+Given an integer array A of length N, consider the last element as the pivot p.
+Rearrange the elements of the array such that for all i:
+
+  - If A[i] < p, then it should be present on the left side of the partition.
+  - If A[i] > p, then it should be present on the right side of the partition.
+
+Rearrange the given array and also return the partition index.
+
+Note:
+  - All elements in the array are distinct.
+
+Problem Constraints
+  1 <= N <= 10^5
+  1 <= A[i] <= 10^9
+
+Input Format
+  The only input argument is the integer array A.
+
+Output Format
+  Rearrange the array to satisfy the given conditions and
+  return the partition index.
+
+Example Input
+
+Input:
+  A = [6, 2, 0, 4, 5]
+
+Example Output
+
+Output:
+  Valid
+
+  A possible solution can be:
+    A = [2, 0, 4, 5, 6]
+    partitionIndex = 3
+
+Example Explanation
+
+Explanation:
+  The rearrangement is valid because every element in the index range [0, 2]
+  is less than the pivot element, and every element in the index range [3, 4]
+  is greater than the pivot element.
+*/
