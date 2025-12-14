7d6526f8c7ff86c68d87a7c187c9169b8eb8b57d
Sai Santhosh Reddy Singareddy <109932454+saisanthoshreddysingareddy@users.noreply.github.com>
2025-12-14 11:48:03 +0530

    Sort the array using quick sort

--- Merge, Count, Quick Sorts/QuickSort.java
+++ Merge, Count, Quick Sorts/QuickSort.java
@@ -34,3 +34,40 @@ Example Explanation
 Explanation 1 and 2:
   Return the sorted array.
 */
+
+public class Solution {
+    public ArrayList<Integer> solve(ArrayList<Integer> A) {
+        quickSort(A, 0, A.size()-1);
+        return A;
+    }
+    public void quickSort(ArrayList<Integer> A, int s, int e){
+        if(s>=e){
+            return;
+        }
+        int pivotIndex = partition(A, s, e);
+        quickSort(A, s, pivotIndex-1);
+        quickSort(A, pivotIndex+1, e);
+    }
+    public int partition(ArrayList<Integer> A, int s, int e){
+        int pivot_element = A.get(s);
+        int l = s+1;
+        int r = e;
+        while(l<=r){
+            if(A.get(l)<=pivot_element){
+                l++;  
+            }else if(A.get(r)>pivot_element){
+                r--;
+            }else{
+                int temp = A.get(r);
+                A.set(r, A.get(l));
+                A.set(l, temp);
+                l++;
+                r--;
+            }
+        }
+        int temp = A.get(s);
+        A.set(s, A.get(r));
+        A.set(r, temp);
+        return r;
+    }
+}
