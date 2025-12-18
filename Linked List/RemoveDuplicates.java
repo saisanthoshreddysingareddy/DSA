b0caa7e810f474fb900d4df09eaa9ab46c897cbf
Sai Santhosh Reddy Singareddy <109932454+saisanthoshreddysingareddy@users.noreply.github.com>
2025-12-18 20:59:23 +0530

    Create RemoveDuplicates.java

---
+++ Linked List/RemoveDuplicates.java
@@ -0,0 +1,39 @@
+/*
+Problem Description
+
+Given a sorted linked list, delete all duplicates such that
+each element appears only once.
+
+Problem Constraints
+  0 <= length of linked list <= 10^6
+
+Input Format
+  The first argument is the head pointer of the linked list.
+
+Output Format
+  Return the head pointer of the linked list after removing
+  all duplicate elements.
+
+Example Input
+
+Input 1:
+  1 -> 1 -> 2
+
+Input 2:
+  1 -> 1 -> 2 -> 3 -> 3
+
+Example Output
+
+Output 1:
+  1 -> 2
+
+Output 2:
+  1 -> 2 -> 3
+
+Example Explanation
+
+Explanation 1:
+  After removing duplicates, each element appears only once,
+  resulting in the linked list:
+    1 -> 2
+*/
