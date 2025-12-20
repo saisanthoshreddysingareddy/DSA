95a2c4610f36f4f54a2cef4646598a01f845881d
Sai Santhosh Reddy Singareddy <109932454+saisanthoshreddysingareddy@users.noreply.github.com>
2025-12-20 20:52:37 +0530

    Create NextGreater.java

---
+++ Stacks/NextGreater.java
@@ -0,0 +1,56 @@
+/*
+Problem Description
+
+Given an array A, find the next greater element G[i] for every element A[i]
+in the array.
+
+The next greater element for an element A[i] is the first greater element
+on the right side of A[i] in the array.
+
+More formally:
+
+G[i] for an element A[i] is an element A[j] such that:
+  - j is the minimum possible index
+  - j > i
+  - A[j] > A[i]
+
+If no such element exists, consider G[i] = -1.
+
+Problem Constraints
+  1 <= |A| <= 10^5
+  1 <= A[i] <= 10^7
+
+Input Format
+  The first and only argument of input contains the integer array A.
+
+Output Format
+  Return an integer array representing the next greater element
+  for each index in A.
+
+Example Input
+
+Input 1:
+  A = [4, 5, 2, 10]
+
+Input 2:
+  A = [3, 2, 1]
+
+Example Output
+
+Output 1:
+  [5, 10, 10, -1]
+
+Output 2:
+  [-1, -1, -1]
+
+Example Explanation
+
+Explanation 1:
+  For 4, the next greater element on the right is 5.
+  For 5 and 2, the next greater element on the right is 10.
+  For 10, there is no greater element on the right, so G[i] = -1.
+
+Explanation 2:
+  Since the array is in descending order, there is no next greater
+  element for any of the elements.
+*/
