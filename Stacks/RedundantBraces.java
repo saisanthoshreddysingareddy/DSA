6f0ceb6f90e02c0d3c86cf10d35a96a7ae7b8671
Sai Santhosh Reddy Singareddy <109932454+saisanthoshreddysingareddy@users.noreply.github.com>
2025-12-20 20:54:49 +0530

    Create RedundantBraces.java

---
+++ Stacks/RedundantBraces.java
@@ -0,0 +1,49 @@
+/*
+Problem Description
+
+Given a string A denoting an arithmetic expression.
+The expression contains the following operators:
+  '+', '-', '*', '/'
+
+Check whether the expression A contains redundant braces or not.
+
+NOTE:
+  - A is always a valid expression.
+  - The expression does not contain any white spaces.
+
+Problem Constraints
+  1 <= |A| <= 10^5
+
+Input Format
+  The only argument given is the string A.
+
+Output Format
+  Return 1 if the expression contains redundant braces.
+  Otherwise, return 0.
+
+Example Input
+
+Input 1:
+  A = "((a+b))"
+
+Input 2:
+  A = "(a+(a+b))"
+
+Example Output
+
+Output 1:
+  1
+
+Output 2:
+  0
+
+Example Explanation
+
+Explanation 1:
+  The expression "((a+b))" contains redundant braces,
+  so the output is 1.
+
+Explanation 2:
+  The expression "(a+(a+b))" does not contain any redundant
+  braces, so the output is 0.
+*/
