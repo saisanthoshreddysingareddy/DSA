86620f35fec40531934549803ba3719d2064304c
Sai Santhosh Reddy Singareddy <109932454+saisanthoshreddysingareddy@users.noreply.github.com>
2025-12-21 17:22:00 +0530

    Create LargestRectangleInHistogram.java

---
+++ Stacks/LargestRectangleInHistogram.java
@@ -0,0 +1,46 @@
+/*
+Problem Description
+
+Given an array of integers A.
+
+The array A represents a histogram, where A[i] denotes the height
+of the i-th bar in the histogram. The width of each bar is 1.
+
+Your task is to find the area of the largest rectangle that can be
+formed within the histogram.
+
+Problem Constraints
+  1 <= |A| <= 100000
+  1 <= A[i] <= 10000
+
+Input Format
+  The only argument given is the integer array A.
+
+Output Format
+  Return the area of the largest rectangle in the histogram.
+
+Example Input
+
+Input 1:
+  A = [2, 1, 5, 6, 2, 3]
+
+Input 2:
+  A = [2]
+
+Example Output
+
+Output 1:
+  10
+
+Output 2:
+  2
+
+Example Explanation
+
+Explanation 1:
+  The largest rectangle has an area of 10 units,
+  formed using bars with heights 5 and 6.
+
+Explanation 2:
+  The largest rectangle has an area of 2.
+*/
