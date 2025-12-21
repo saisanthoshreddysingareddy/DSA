e37a5b13079aaebe5387c3ad4029274b96bc0e10
Sai Santhosh Reddy Singareddy <109932454+saisanthoshreddysingareddy@users.noreply.github.com>
2025-12-21 17:29:50 +0530

    Create SortStackUsingAnotherStack.java

---
+++ Stacks/SortStackUsingAnotherStack.java
@@ -0,0 +1,43 @@
+/*
+Problem Description
+
+Given a stack of integers A, sort the stack using another stack.
+
+Return the array of integers after sorting the stack using
+an additional stack.
+
+Problem Constraints
+  1 <= |A| <= 5000
+  0 <= A[i] <= 10^9
+
+Input Format
+  The only argument is a stack given as an integer array A.
+
+Output Format
+  Return the array of integers after sorting the stack
+  using another stack.
+
+Example Input
+
+Input 1:
+  A = [5, 4, 3, 2, 1]
+
+Input 2:
+  A = [5, 17, 100, 11]
+
+Example Output
+
+Output 1:
+  [1, 2, 3, 4, 5]
+
+Output 2:
+  [5, 11, 17, 100]
+
+Example Explanation
+
+Explanation 1:
+  The given stack is sorted in ascending order.
+
+Explanation 2:
+  The given stack is sorted in ascending order.
+*/
