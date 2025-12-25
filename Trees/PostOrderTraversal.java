58c0ea0eb3d44590bc0a1932be928461fccfcb04
Sai Santhosh Reddy Singareddy <109932454+saisanthoshreddysingareddy@users.noreply.github.com>
2025-12-25 20:07:42 +0530

    Create PostOrderTraversal.java

---
+++ Trees/PostOrderTraversal.java
@@ -0,0 +1,56 @@
+/*
+Problem Description
+
+Given a binary tree, return the postorder traversal of its nodes' values.
+
+
+Problem Constraints
+
+1 <= number of nodes <= 10^5
+
+
+Input Format
+
+The first and only argument is the root node of the binary tree A.
+
+
+Output Format
+
+Return an integer array denoting the postorder traversal of the given
+binary tree.
+
+
+Example Input
+
+Input 1:
+        1
+         \
+          2
+         /
+        3
+
+Input 2:
+        1
+       / \
+      6   2
+         /
+        3
+
+
+Example Output
+
+Output 1:
+[3, 2, 1]
+
+Output 2:
+[6, 3, 2, 1]
+
+
+Example Explanation
+
+Explanation 1:
+The postorder traversal of the given tree is [3, 2, 1].
+
+Explanation 2:
+The postorder traversal of the given tree is [6, 3, 2, 1].
+*/
