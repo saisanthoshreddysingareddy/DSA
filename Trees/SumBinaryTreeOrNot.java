36c0afa17b16d79290fa2fd60644b1abd209d760
Sai Santhosh Reddy Singareddy <109932454+saisanthoshreddysingareddy@users.noreply.github.com>
2025-12-25 20:08:22 +0530

    Create SumBinaryTreeOrNot.java

---
+++ Trees/SumBinaryTreeOrNot.java
@@ -0,0 +1,70 @@
+/*
+Problem Description
+
+Given a binary tree, check whether the given tree is a Sum-binary Tree
+or not.
+
+A Sum-binary Tree is a binary tree where the value of every node is equal
+to the sum of the values of nodes present in its left subtree and right
+subtree.
+
+An empty tree is considered a Sum-binary Tree, and the sum of an empty
+tree is considered as 0. A leaf node is also considered a Sum-binary Tree.
+
+Return 1 if the given tree is a Sum-binary Tree, else return 0.
+
+
+Problem Constraints
+
+1 <= number of nodes <= 100000
+0 <= node values <= 50
+
+
+Input Format
+
+The first and only argument is the root node of the binary tree A.
+
+
+Output Format
+
+Return 1 if the tree is a Sum-binary Tree, else return 0.
+
+
+Example Input
+
+Input 1:
+            26
+          /    \
+         10     3
+        /  \     \
+       4    6     3
+
+Input 2:
+            26
+          /    \
+         10     3
+        /  \     \
+       4    6     4
+
+
+Example Output
+
+Output 1:
+1
+
+Output 2:
+0
+
+
+Example Explanation
+
+Explanation 1:
+All leaf nodes are considered Sum-binary Trees.
+Value of node 10 = 4 + 6.
+Value of node 3 = 0 + 3.
+Value of node 26 = (10 + 4 + 6) + 6.
+
+Explanation 2:
+The sum of the left subtree and right subtree is 27, which is not equal
+to the value of the root node (26).
+*/
