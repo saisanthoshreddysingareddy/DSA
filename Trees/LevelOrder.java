511213d7cabfc61e84aacdb8625df6939bc63ceb
Sai Santhosh Reddy Singareddy <109932454+saisanthoshreddysingareddy@users.noreply.github.com>
2025-12-25 20:04:50 +0530

    Create LevelOrder.java

---
+++ Trees/LevelOrder.java
@@ -0,0 +1,63 @@
+/*
+Problem Description
+
+Given a binary tree, return the level order traversal of its nodes' values
+(i.e., from left to right, level by level).
+
+
+Problem Constraints
+
+1 <= number of nodes <= 10^5
+
+
+Input Format
+
+The first and only argument is the root node of the binary tree A.
+
+
+Output Format
+
+Return a 2D integer array denoting the level order traversal of the given
+binary tree.
+
+
+Example Input
+
+Input 1:
+        3
+       / \
+      9  20
+        /  \
+       15   7
+
+Input 2:
+        1
+       / \
+      6   2
+         /
+        3
+
+
+Example Output
+
+Output 1:
+[
+  [3],
+  [9, 20],
+  [15, 7]
+]
+
+Output 2:
+[
+  [1],
+  [6, 2],
+  [3]
+]
+
+
+Example Explanation
+
+Explanation 1:
+Return the 2D array where each row represents the nodes present at
+that level of the binary tree, traversed from left to right.
+*/
