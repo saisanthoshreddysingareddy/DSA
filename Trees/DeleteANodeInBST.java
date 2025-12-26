823b2a1df2c4012487debfa86bb1962807b0d4a4
Sai Santhosh Reddy Singareddy <109932454+saisanthoshreddysingareddy@users.noreply.github.com>
2025-12-26 18:30:06 +0530

    Create DeleteANodeInBST.java

---
+++ Trees/DeleteANodeInBST.java
@@ -0,0 +1,77 @@
+/*
+Problem Description
+
+Given a Binary Search Tree (BST) A, if there is a node with value B
+present in the tree, delete it and return the modified tree.
+
+NOTE:
+If there are multiple valid ways to delete the node, always replace
+the node with its in-order predecessor.
+
+
+Problem Constraints
+
+2 <= number of nodes in the BST <= 10^5
+1 <= value of nodes <= 10^9
+Each node has a unique value
+
+
+Input Format
+
+The first argument is the root node of the Binary Search Tree A.
+The second argument is the integer value B to be deleted.
+
+
+Output Format
+
+Delete the given node if found and return the root node of the BST.
+
+
+Example Input
+
+Input 1:
+                15
+              /    \
+            12      20
+            / \    /  \
+           10  14  16  27
+          /
+         8
+
+B = 10
+
+Input 2:
+                8
+               / \
+              6   21
+             / \
+            1   7
+
+B = 6
+
+
+Example Output
+
+Output 1:
+                15
+              /    \
+            12      20
+            / \    /  \
+           8   14  16  27
+
+Output 2:
+                8
+               / \
+              1   21
+               \
+                7
+
+
+Example Explanation
+
+Explanation 1:
+The node with value 10 is deleted from the Binary Search Tree.
+
+Explanation 2:
+The node with value 6 is deleted from the Binary Search Tree.
+*/
