d00399e66b68b8d66869bdd5bc61aade8d8459ae
Sai Santhosh Reddy Singareddy <109932454+saisanthoshreddysingareddy@users.noreply.github.com>
2025-12-26 18:29:38 +0530

    Create SortedArrayToBalancedBST.java

---
+++ Trees/SortedArrayToBalancedBST.java
@@ -0,0 +1,52 @@
+/*
+Problem Description
+
+Given an array where the elements are sorted in ascending order, convert
+it into a height-balanced Binary Search Tree (BST).
+
+A height-balanced binary tree is defined as a binary tree in which the
+depth of the two subtrees of every node never differ by more than 1.
+
+Problem Constraints
+
+1 <= length of the array <= 100000
+
+Input Format
+
+The first argument is an integer array A.
+
+
+Output Format
+
+Return the root node of the Binary Search Tree.
+
+
+Example Input
+
+Input 1:
+A = [1, 2, 3]
+
+Input 2:
+A = [1, 2, 3, 5, 10]
+
+
+Example Output
+
+Output 1:
+            2
+          /   \
+         1     3
+
+Output 2:
+            3
+          /   \
+         2     5
+        /       \
+       1         10
+
+Example Explanation
+
+Explanation 1:
+You need to return the root node of a height-balanced Binary Search Tree
+constructed from the given sorted array.
+*/
